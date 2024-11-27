447810f1d4b6f20f276c497a7c78e800d2b8ac2b
c2keesey <c2keesey@gmail.com>
2024-11-26 18:04:04 -0800

    color palette docs

--- main/src/effects/shared/ColorPalette.h
+++ main/src/effects/shared/ColorPalette.h
@@ -20,6 +20,13 @@ private:
     bool isTwoColorSpread = true;
 
 public:
+    /**
+     * @brief Constructs a ColorPalette with the given colors and variance parameters
+     * @param colorList Vector of CHSV colors to use in the palette
+     * @param hueVar Amount of random variance to apply to hue (default: 5)
+     * @param satVar Amount of random variance to apply to saturation (default: 5)
+     * @param valVar Amount of random variance to apply to value/brightness (default: 5)
+     */
     ColorPalette(const std::vector<CHSV> &colorList, int hueVar = 5, int satVar = 5, int valVar = 5)
     {
         hueVariance = hueVar;
@@ -37,6 +44,11 @@ public:
         isTwoColorSpread = (colors.size() == 2);
     }
 
+    /**
+     * @brief Gets a color at the specified index without any random variance
+     * @param index Index of the color to retrieve
+     * @return The exact CHSV color at the index, or black if index is invalid
+     */
     CHSV getExactColor(size_t index) const
     {
         if (index < colors.size())
@@ -46,6 +58,10 @@ public:
         return CHSV(0, 0, 0);
     }
 
+    /**
+     * @brief Gets a random color from the palette without any variance
+     * @return A random CHSV color from the palette, or black if palette is empty
+     */
     CHSV getExactRandomColor() const
     {
         if (!colors.empty())
@@ -56,11 +72,20 @@ public:
         return CHSV(0, 0, 0);
     }
 
+    /**
+     * @brief Gets a random color blended between the first two colors in the palette
+     * @return A CHSV color randomly blended between colors[0] and colors[1]
+     */
     CHSV getRandomSpreadTwoColor() const
     {
         return getBlendedColor(0, 1, random8());
     }
 
+    /**
+     * @brief Gets a color at the specified index with random variance applied
+     * @param index Index of the color to retrieve
+     * @return The CHSV color at the index with variance, or black if index is invalid
+     */
     CHSV getColor(size_t index)
     {
         if (index < colors.size())
@@ -82,6 +107,11 @@ public:
         return CHSV(0, 0, 0);
     }
 
+    /**
+     * @brief Gets a random color from the palette with variance
+     * For two-color palettes, returns a blend between the colors
+     * @return A random CHSV color (with variance) from the palette, or black if empty
+     */
     CHSV getRandomColor()
     {
         if (colors.size() == 2 && isTwoColorSpread)
@@ -96,6 +126,10 @@ public:
         return CHSV(0, 0, 0);
     }
 
+    /**
+     * @brief Gets a new random index different from the last used index
+     * @return A random index into the color palette
+     */
     size_t getNewRandomIndex()
     {
         if (colors.size() == 1)
@@ -110,6 +144,13 @@ public:
         return randomIndex;
     }
 
+    /**
+     * @brief Blends two colors from the palette by the specified amount
+     * @param index1 Index of the first color
+     * @param index2 Index of the second color
+     * @param blendAmt Amount to blend (0 = all color1, 255 = all color2)
+     * @return The blended CHSV color, or black if either index is invalid
+     */
     CHSV getBlendedColor(size_t index1, size_t index2, uint8_t blendAmt) const
     {
         if (index1 < colors.size() && index2 < colors.size())
@@ -125,17 +166,30 @@ public:
         return CHSV(0, 0, 0);
     }
 
+    /**
+     * @brief Gets the next color in sequence with variance applied
+     * @return The next CHSV color in the palette rotation
+     */
     CHSV getNextColor()
     {
         u8_t nextIndex = (lastIndex + 1) % colors.size();
         return getColor(nextIndex);
     }
 
+    /**
+     * @brief Returns the number of colors in the palette
+     * @return Size of the color palette
+     */
     size_t size() const
     {
         return colors.size();
     }
 
+    /**
+     * @brief Rotates the hue of a single-color palette
+     * Only works if the palette contains exactly one color
+     * @param index The new hue value (modulo 255)
+     */
     void rotate(int index)
     {
         if (size() != 1)
